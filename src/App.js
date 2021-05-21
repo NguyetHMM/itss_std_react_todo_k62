06721b09ea814b50c401e8798041ebbbed00b635
nguyethm <nguyet332747@gmail.com>
2021-05-21 16:17:46 +0700

    Save Avatar By Storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }  from 'react'
+import React, { useEffect, useState } from 'react'
 
 /* スタイルシート */
 import './styles/main.css';
@@ -6,7 +6,8 @@ import './styles/main.css';
 /* コンポーネント */
 import Todo from './components/Todo';
 import Login from "./components/Login";
-import { auth, storeUserInfo } from "./lib/firebase";
+import Upload from "./components/Upload";
+import { auth, storeUserInfo, updateUser } from "./lib/firebase";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -27,26 +28,31 @@ function App() {
     auth.signOut();
   };
 
-    const HeaderContent = () => {
-      if (user) {
-        return (
-          <div class="navbar-end">
-            <div class="navbar-item">
-              {user.name}
-            </div>
-            <div class="navbar-item">
-              <button class="button is-danger is-light is-small" onClick={logout} > Logout</button>
-            </div>
-          </div >
-        )
-      } else {
-        return (<Login />)
-      }
+  const handleImageChanged = async downlodUrl => {
+    await updateUser(user, downlodUrl);
+  }
+
+  const HeaderContent = () => {
+    if (user) {
+      return (
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <Upload userImage={user.image} onSletctedImage={handleImageChanged} />
+            {user.name}
+          </div>
+          <div class="navbar-item">
+            <button class="button is-danger is-light is-small" onClick={logout} > Logout</button>
+          </div>
+        </div >
+      )
+    } else {
+      return (<Login />)
     }
-  
+  }
+
   return (
     <div className="container is-fluid">
-       <header class="navbar">
+      <header class="navbar">
         {loading ? (
           <p>
             LOADING.....
